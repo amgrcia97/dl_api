c5be35011edb5a7f6e5796fca007085291aba8f7
amgrcia97 <amgrcia97@gmail.com>
2023-08-21 14:43:43 -0300

    ok

--- discere_linguis_api/settings/dev.py
+++ discere_linguis_api/settings/dev.py
@@ -22,8 +22,12 @@ ROOT_URLCONF = 'discere_linguis_api.urls'
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2']
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'discere_linguis_dev',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'discerelinguis$dl_dev',
+        'USER': 'discerelinguis',
+        'PASSWORD': '@OwP998M0*Z$',
+        'HOST': 'discerelinguis.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 # # Amazon AWS
